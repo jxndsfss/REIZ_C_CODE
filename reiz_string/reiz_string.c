9b4741f1d9f9c75ed390f21148a43831f4ca1390
chenlei <1005076009@qq.com>
2018-12-20 00:01:21 +0800

    new

---
+++ reiz_string/reiz_string.c
@@ -0,0 +1,91 @@
+/*
+    @file     reiz_string.c
+    @author   jxndsfss
+    @version  v1.0.0
+    @date     2018-12-06
+    @site     ShangYouSong.SZ
+    @brief    字符串相关函数实现源文件
+*/
+
+/* includes ------------------------------------------------------------------*/
+#include "reiz_string.h"
+
+/* private define ------------------------------------------------------------*/
+/* private macro -------------------------------------------------------------*/
+/* private typedef -----------------------------------------------------------*/
+/* private variables ---------------------------------------------------------*/
+/* exported variables --------------------------------------------------------*/
+/* private function prototypes -----------------------------------------------*/
+
+/*
+    @brief  删除字符串中所有出现的某个字符
+    @param  pDes - 被处理的字符串
+    @param  c    - 需要删除的字符
+    @return void
+*/
+void deleteCharInStr_rz(char *pDes, char c){
+    char *pStore = pDes;
+
+    while (*pDes != '\0') {
+        if (*pDes != c) {
+            *pStore++ = *pDes；
+        }
+        pDes++;
+    }
+    *pStore = '\0';
+}
+
+/*
+    @brief  删除字符串pDes中所有在字符串pSearch中出现的字符
+    @param  pDes    - 被处理的字符串
+    @param  pSearch - 包含所有需要删除的字符的字符串，供搜索比较
+    @return void
+*/
+void deleteSomeCharInStr_rz(char *pDes, char *pSearch){
+    char *pStore = pDes, *pModify;
+
+    while (*pDes != '\0') {
+        pModify = pSearch;
+
+        while (*pModify != '\0') {
+            if (*pDes == *pModify) {
+                break;
+            }
+            pModify++;
+        }
+
+        if (*pModify != '\0') {
+            pDes++;
+            continue;
+        }
+        *pStore++ = *pDes++;
+    }
+    *pStore = '\0';
+}
+
+/*
+    @brief  将字符串中出现的小写字母转成大写字母
+    @param  pStr - 字符串指针
+    @return void
+*/
+void strToUpper_rz(char *pStr){
+    while (*pStr != '\0' && *pStr >= 'a' && *pStr <= 'z') {
+        *pStr++ -= 'a' - 'A';
+    }
+}
+
+/*
+    @brief  将字符串中出现的大写字母转成小写字母
+    @param  pStr - 字符串指针
+    @return void
+*/
+void strToLower_rz(char *pStr){
+    int i = 0;
+
+    while (*pStr != '\0' && *pStr >= 'A' && *pStr <= 'Z') {
+        *pStr++ += 'a' - 'A';
+    }
+}
+
+/******************************** END OF FILE **********************************
+*************************** (C) Copyright 2018 REIZ ***************************/
---
+++ reiz_string/reiz_string.h
@@ -0,0 +1,40 @@
+/*
+    @file     reiz_string.h
+    @author   jxndsfss
+    @version  v1.0.0
+    @date     2018-12-06
+    @site     ShangYouSong.SZ
+    @brief    字符串相关函数实现头文件
+*/
+
+/* define to prevent recursive inclusion -------------------------------------*/
+#ifndef REIZ_STRING_H
+#define REIZ_STRING_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* includes ------------------------------------------------------------------*/
+#include <stdbool.h>
+
+/* exported define -----------------------------------------------------------*/
+/* exported macro ------------------------------------------------------------*/
+/* exported types ------------------------------------------------------------*/
+/* exported variables --------------------------------------------------------*/
+
+/* exported functions prototypes ---------------------------------------------*/
+extern void deleteCharInStr_rz(char *pDes, char c);
+extern void deleteSomeCharInStr_rz(char *pDes, char *pSearch);
+extern void strToUpper_rz(char *pStr);
+extern void strToLower_rz(char *pStr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* REIZ_STRING_H */
+
+/******************************** END OF FILE **********************************
+*************************** (C) Copyright 2018 REIZ ***************************/
