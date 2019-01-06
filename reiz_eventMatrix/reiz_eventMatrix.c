f9e5d3ba53e53f33b7a2e2a822b02a557f161e1c
chenlei <1005076009@qq.com>
2019-01-06 22:46:30 +0800

    v1.0.1

--- reiz_eventMatrix/reiz_eventMatrix.c
+++ reiz_eventMatrix/reiz_eventMatrix.c
@@ -1,69 +1,152 @@
-/*
-    @file       reiz_eventMatrix.c
-    @author     jxndsfss
-    @version    v1.0.0
-    @date       2018-12-25
-    @site       ShangYouSong.SZ
-    @brief      事件矩阵实现源文件
-*/
+/*******************************************************************************
+ *  @file       reiz_eventMatrix.c
+ *  @author     jxndsfss
+ *  @version    v1.0.1
+ *  @date       2019-01-06
+ *  @site       ShangYouSong.SZ
+ *  @brief      事件矩阵实现源文件
+ *******************************************************************************
+ */
 
-/*----- includes -------------------------------------------------------------*/
+/* Includes ------------------------------------------------------------------*/
 #include "reiz_eventMatrix.h"
 
-/*----- private define -------------------------------------------------------*/
-/*----- private macro --------------------------------------------------------*/
-/*----- private typedef ------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private typedef -----------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Exported variables --------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
 
-/*----- private variables ----------------------------------------------------*/
-uint32_t eventMatrix[MATRIX_ROW] = {0};
+/*******************************************************************************
+ *  @brief  设置事件标志
+ *  @param  pEcb      - 事件控制块指针
+ *          eventName - 事件标志
+ *  @return true      - 设置成功
+ *          false     - 设置失败
+ */
+extern bool eventMatrix_SetEventFlag(pEcb_t pEcb, int eventFlag) {
+    int row, col;
 
-/*----- exported variables ---------------------------------------------------*/
-/*----- private function prototypes ------------------------------------------*/
-
-/*
-    @brief  读取事件标志
-    @param  row - 事件所在矩阵行数
-    @param  col - 事件所在矩阵列数
-    @return true  - 事件存在
-    @return false - 事件不存在
-*/
-extern bool getEventFlag(int row, int col) {
-    return eventMatrix[row] & (1 << col);
-}
-
-/*
-    @brief  设置事件标志
-    @param  row - 事件所在矩阵行数
-    @param  col - 事件所在矩阵列数
-    @return void
-*/
-extern void setEventFlag(int row, int col) {
-    eventMatrix[row] |= (1 << col);
-}
-
-/*
-    @brief  清除事件标志
-    @param  row - 事件所在矩阵行数
-    @param  col - 事件所在矩阵列数
-    @return void
-*/
-extern void clearEventFlag(int row, int col) {
-    eventMatrix[row] &= ~(1 << col);
-}
-
-/*
-    @brief  事件矩阵是否为空，即有无事件存在
-    @param  void
-    @return true  - 矩阵为空，无事件存在
-    @return false - 矩阵不为空，有事件存在
-*/
-extern bool eventMatrixIsEmpty(void) {
-    for (int i = 0; i < MATRIX_ROW; i++) {
-        if (eventMatrix[i])
-            return false;
+    if (pEcb != NULL) {
+        row = eventFlag / (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        col = eventFlag % (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        pEcb->flagMatrix[row] |= (FLAG_MATRIX_ROW_TYPE)1 << col;
+        return true;
     }
-    return true;
+    return false;
+}
+
+/*******************************************************************************
+ *  @brief  读取事件标志
+ *  @param  pEcb      - 事件控制块指针
+ *          eventName - 事件标志
+ *  @return true      - 事件标志存在
+ *          false     - 事件标志不存在
+ */
+extern bool eventMatrix_GetEventFlag(pEcb_t pEcb, int eventFlag) {
+    int row, col;
+
+    if (pEcb != NULL) {
+        row = eventFlag / (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        col = eventFlag % (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        return pEcb->flagMatrix[row] & ((FLAG_MATRIX_ROW_TYPE)1 << col) ? true : false;        
+    }
+    return false;
+}
+
+/*******************************************************************************
+ *  @brief  清除事件标志
+ *  @param  pEcb      - 事件控制块指针
+ *          eventName - 事件标志
+ *  @return true      - 清除成功
+ *          false     - 清除失败
+ */
+extern bool eventMatrix_ClearEventFlag(pEcb_t pEcb, int eventFlag) {
+    int row, col;
+
+    if (pEcb != NULL) {
+        row = eventFlag / (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        col = eventFlag % (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        pEcb->flagMatrix[row] &= ~((FLAG_MATRIX_ROW_TYPE)1 << col);
+        return true;
+    }
+    return false;
+}
+
+/*******************************************************************************
+ *  @brief  事件处理函数，轮询事件矩阵
+ *  @param  pEcb - 事件控制块指针
+ *  @return void
+ */
+extern void eventMatrix_EventProcess(pEcb_t pEcb) {
+    int row, col, flag;
+
+    if (pEcb == NULL) {
+        return;
+    }
+
+    for (row = 0; row < MATRIX_ROW; row++) {
+        if (!pEcb->flagMatrix[row]) {                                           //该行无事件则跳过，继续下一行
+            continue;        
+        }
+
+        for (col = 0; col < sizeof(FLAG_MATRIX_ROW_TYPE) * 8; col++) {
+            flag = pEcb->flagMatrix[row] & ((FLAG_MATRIX_ROW_TYPE)1 << col);    //获得事件标志
+
+            if (flag && pEcb->CbMatrix[row][col] != NULL) {                     //事件标志存在且回调函数存在则执行事件处理回调函数
+                pEcb->CbMatrix[row][col](pEcb->paraMatrix[row][col]);
+
+                if (pEcb->paraMatrix[row][col] != NULL) {                       //检查参数集合数据结构内存是否释放，未释放则进行释放
+                    free(pEcb->paraMatrix[row][col]);
+                    pEcb->paraMatrix[row][col] = NULL;
+                }
+                pEcb->flagMatrix[row] &= ~((FLAG_MATRIX_ROW_TYPE)1 << col);     //清除事件标志
+            }
+        }
+    }
+}
+
+/*******************************************************************************
+ *  @brief  保存事件参数集合数据结构指针，以供事件处理回调函数使用
+ *  @param  pEcb      - 事件控制块指针
+ *          eventFlag - 事件标志
+ *          pPara     - 事件参数集合数据结构指针
+ *  @return true      - 保存成功
+ *          false     - 保存失败
+ */
+extern bool eventMatrix_SaveEventPara(pEcb_t pEcb, int eventFlag, pPara_t pPara) {
+    int row, col;
+
+    if (pEcb != NULL) {
+        row = eventFlag / (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        col = eventFlag % (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        pEcb->paraMatrix[row][col] = pPara;
+        return true;
+    }
+    return false;
+}
+
+/*******************************************************************************
+ *  @brief  注册事件处理回调函数
+ *  @param  pEcb      - 事件控制块指针
+ *          eventFlag - 事件标志
+ *          pCb       - 事件处理回调函数指针
+ *  @return true      - 注册成功
+ *          false     - 注册失败
+ */
+extern bool eventMatrix_RegistEventHandleCB(pEcb_t pEcb, int eventFlag, pEventCB_t pCb) {
+    int row, col;
+
+    if (pEcb != NULL) {
+        row = eventFlag / (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+        col = eventFlag % (sizeof(FLAG_MATRIX_ROW_TYPE) * 8);
+
+        pEcb->CbMatrix[row][col] = pCb;
+        return true;
+    }
+    return false;
 }
 
 /******************************** END OF FILE **********************************
-*************************** (C) Copyright 2018 REIZ ***************************/
+*************************** (C) Copyright 2019 REIZ ***************************/
--- reiz_eventMatrix/reiz_eventMatrix.h
+++ reiz_eventMatrix/reiz_eventMatrix.h
@@ -1,18 +1,14 @@
-/*
-    @file       reiz_eventMatrix.h
-    @author     jxndsfss
-    @version    v1.0.0
-    @date       2018-12-25
-    @site       ShangYouSong.SZ
-    @brief      事件矩阵实现头文件
+/*******************************************************************************
+ *  @file       reiz_eventMatrix.h
+ *  @author     jxndsfss
+ *  @version    v1.0.1
+ *  @date       2019-01-06
+ *  @site       ShangYouSong.SZ
+ *  @brief      事件矩阵实现头文件
+ *******************************************************************************
+ */
 
-    @usecase    宏作为函数的参数，宏中包含矩阵位置的行与列参数
-                getEventFlag(EVENT_1_FLAG);
-                setEventFlag(EVENT_4_FLAG);
-                clearEventFlag(EVENT_31_FLAG);
-*/
-
-/*----- define to prevent recursive inclusion --------------------------------*/
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef REIZ_EVENT_MATRIX_H
 #define REIZ_EVENT_MATRIX_H
 
@@ -21,41 +17,108 @@ extern "C"
 {
 #endif
 
-/*----- includes -------------------------------------------------------------*/
+/* Includes ------------------------------------------------------------------*/
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-/*----- exported define ------------------------------------------------------*/
+/* Exported define -----------------------------------------------------------*/
 
-/* 事件矩阵行数，所能表示总事件数为 MATRIX_ROW*32，事件矩阵用于事件较多的场合 */
-#define MATRIX_ROW          3                   
+/* 
+ * 事件矩阵行数，所能表示总事件数为 MATRIX_ROW * sizeof(FLAG_MATRIX_ROW_TYPE) * 8，
+ * 事件矩阵用于事件较多的场合，举例若 FLAG_MATRIX_ROW_TYPE 为 uint32_t，
+ * 则事件总数为MATRIX_ROW * 32，若MATRIX_ROW为2，则事件总数为64
+ */
+#define MATRIX_ROW                  1
 
-/* 注：事件矩阵列数COL最大为31 */
-#define EVENT_0_FLAG        0, 0
-#define EVENT_1_FLAG        0, 1
-#define EVENT_2_FLAG        0, 2
-#define EVENT_3_FLAG        1, 3
-#define EVENT_4_FLAG        1, 4
-#define EVENT_5_FLAG        1, 5
-#define EVENT_29_FLAG       2, 29
-#define EVENT_30_FLAG       2, 30
-#define EVENT_31_FLAG       2, 31
+/* 事件标志矩阵一维数组元素类型 */
+#define FLAG_MATRIX_ROW_TYPE        uint32_t
 
-/*----- exported macro -------------------------------------------------------*/
-/*----- exported types -------------------------------------------------------*/
-/*----- exported variables ---------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
 
-/*----- exported functions prototypes ----------------------------------------*/
-extern bool getEventFlag(int row, int col);                 //读取事件标志
-extern void setEventFlag(int row, int col);                 //设置事件标志
-extern void clearEventFlag(int row, int col);               //清除事件标志
-extern bool eventMatrixIsEmpty(void);                       //事件矩阵是否为空
+/* Exported types ------------------------------------------------------------*/
+typedef void (*pEventCB_t)(void *pPara);                                                        //事件处理回调函数指针类型定义
+typedef void *pPara_t;                                                                          //事件处理函数参数构造数据类型指针
+
+typedef struct eventControlBlock_ {
+    FLAG_MATRIX_ROW_TYPE    flagMatrix[ MATRIX_ROW ];                                           //事件标志矩阵
+    pEventCB_t              CbMatrix  [ MATRIX_ROW ][ sizeof(FLAG_MATRIX_ROW_TYPE) * 8 ];       //事件处理回调函数指针矩阵
+    pPara_t                 paraMatrix[ MATRIX_ROW ][ sizeof(FLAG_MATRIX_ROW_TYPE) * 8 ];       //事件参数集合数据结构指针矩阵
+} ecb_t, *pEcb_t;
+
+/* Exported variables --------------------------------------------------------*/
+
+/* Exported functions prototypes ---------------------------------------------*/
+extern void eventMatrix_EventProcess(pEcb_t pEcb);                                              //事件处理函数，轮询事件矩阵
+extern bool eventMatrix_SetEventFlag(pEcb_t pEcb, int eventFlag);                               //设置事件标志
+extern bool eventMatrix_GetEventFlag(pEcb_t pEcb, int eventFlag);                               //读取事件标志
+extern bool eventMatrix_ClearEventFlag(pEcb_t pEcb, int eventFlag);                             //清除事件标志
+extern bool eventMatrix_SaveEventPara(pEcb_t pEcb, int eventFlag, pPara_t pPara);               //保存事件参数集合数据结构指针，以供事件处理回调函数使用
+extern bool eventMatrix_RegistEventHandleCB(pEcb_t pEcb, int eventFlag, pEventCB_t pCb);        //注册事件处理回调函数
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* REIZ_EVENT_MATRIX_H */
+
+/*******************************************************************************
+ *  @usecase
+ *
+ *  app.c
+ *
+ *  //事件名称标志
+ *  #define EVENT_0_FLAG    0
+ *  #define EVENT_1_FLAG    1
+ *  //事件数量可从0开始无限增加...
+ *
+ *  //与事件相关的参数集合，数据类型定义
+ *  typedef struct para_ {
+ *      int data;
+ *      //...
+ *  } para_t;
+ *
+ *  //定义事件控制块及其指针
+ *  static ecb_t ecb;
+ *  extern pEcb_t pEcb = &ecb;
+ *
+ *  //定义事件处理回调函数
+ *  static event_0_CB(void *pPara) {
+ *      //...
+ *      //参数使用完不释放指针pPara指向的内存，由事件处理函数内部进行释放！
+ *  }
+ *
+ *  static event_1_CB(void *pPara) {
+ *      //...
+ *      //参数使用完不释放指针pPara指向的内存，由事件处理函数内部进行释放！
+ *  }
+ *
+ *  //程序运行前初始化，注册所有事件的回调函数
+ *  extern registEventCB(void) {
+ *      eventMatrix_RegistEventHandleCB(pEcb, EVENT_0_FLAG, event_0_CB);
+ *      eventMatrix_RegistEventHandleCB(pEcb, EVENT_1_FLAG, event_1_CB);
+ *  }
+ *
+ *  void main(void) {
+ *      para_t event0Para, event1Para;
+ *
+ *      registEventCB();
+ *
+ *      event1Para.data = 0;
+ *      eventMatrix_SetEventFlag(pEcb, EVENT_0_FLAG);
+ *      eventMatrix_SaveEventPara(pEcb, EVENT_0_FLAG, &event0Para);
+ *
+ *      event1Para.data = 1;
+ *      eventMatrix_SetEventFlag(pEcb, EVENT_1_FLAG);
+ *      eventMatrix_SaveEventPara(pEcb, EVENT_1_FLAG, &event1Para);
+ *
+ *      while(1) {
+ *          eventMatrix_EventProcess(pEcb);
+ *      }
+ *  }
+ *
+ *******************************************************************************
+ */
 
 /******************************** END OF FILE **********************************
 *************************** (C) Copyright 2018 REIZ ***************************/
